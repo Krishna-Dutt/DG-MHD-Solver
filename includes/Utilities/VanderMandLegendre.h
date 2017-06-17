43f2cba3372e4861b0f298693aef38e319d3a0dc
KrishnaDutt <kdutt0592@gmail.com>
2017-06-17 11:18:07 +0530

    Update VanderMandMatrix to accommodate 2D calculation

--- includes/Utilities/VanderMandLegendre.h
+++ includes/Utilities/VanderMandLegendre.h
@@ -1,6 +1,7 @@
 #ifndef VANDERMANDLEGENDRE_H
 #define VANDERMANDLEGENDRE_H
 
+void VanderMandLegendre(double *VanderMandMatrix, unsigned N);
 void twoDVanderMandLegendre(double *VanderMandMatrix, unsigned N);
 
 #endif
--- src/Utilities/VanderMandLegendre.cpp
+++ src/Utilities/VanderMandLegendre.cpp
@@ -6,30 +6,24 @@
 
 using namespace std;
 
-void twoDVanderMandLegendre(double *VanderMandMatrix, unsigned N) {
+void VanderMandLegendre(double *VanderMandMatrix, unsigned N) {
   double *Nodes = new double [N+1];
   double **LegendrePoly;
 
   LegendrePoly = new double*[N+1];
   lobattoNodes(Nodes, N+1);
 
-  unsigned i1,j1,i2,j2;
-  double k;
+  unsigned i,j;
 
   for( i=0; i<=N; ++i) {
     LegendrePoly[i] = new double [i+1];
     legendrePolynomial(LegendrePoly[i], i);
   }
 
-   function<double(double)> eval;
-   for(i1=0;i1<=N;i1++)
+   for(i=0;i<=N;i++)
     {
-        for(j1=0;j1<=N;j1++)
-        {
-          for(i2=0; i2<=N; ++i2)
-            for(j2=0 ; j2<=N; ++j2) {
-              k = sqrt((2.0*i2+1.0) * (2.0*j2+1.0))*0.5;
-              VanderMandMatrix[i*(N+1)+j] = k * polyEval(LegendrePoly[i],i,Nodes[i]) * polyEval(LegendrePoly[j],j,Nodes[j]);
+      for(j=0;j<=N;j++) {
+              VanderMandMatrix[i*(N+1)+j] = polyEval(LegendrePoly[j],j,Nodes[i]);
         }
     }
 
@@ -43,6 +37,26 @@ void twoDVanderMandLegendre(double *VanderMandMatrix, unsigned N) {
 }
 
 
+void twoDVanderMandLegendre(double *VanderMandMatrix, unsigned N) {
+  double VM[(N+1)][(N+1)];
+
+  int i1,i2,j1,j2;
+  double k;
+
+  VanderMandLegendre(*VM, N);
+
+  for(i1=0; i1<=N; ++i1)
+    for(j1=0; j1<=N; ++j1) {
+      for(i2=0; i2<=N; ++i2)
+        for(j2=0; j2<=N; ++j2) {
+          k = sqrt((2.0*i2+1.0) * (2.0*j2+1.0)) * 0.5;
+          VanderMandMatrix[(i1*(N+1)+j1)*(N+1)*(N+1)+i2*(N+1)+j2] = k * VM[i1][i2] * VM[j1][j2];
+        }
+    }
+  return ;
+}
+
+ 
  
 
 
