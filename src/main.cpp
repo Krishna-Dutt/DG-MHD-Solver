6d9e648a26e3defea705e0e2111e4aab36ee5221
KrishnaDutt <kdutt0592@gmail.com>
2017-06-22 11:47:39 +0530

    Results are undershooting, ie; developing at slower pace than standard cases

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,16 @@ using namespace std;
 
 double U(double x, double y) {
   if ( x <= 0.0 && y <= 0.0 ) {
-    return 0.0 ;
+    return -2.0 ;
   }
   else if ( x <= 0.0 && y > 0.0) {
-    return 0.0;
+    return -2.0;
   }
   else if ( x > 0.0 && y > 0.0) {
-    return 0.0 ;
+    return 2.0 ;
   }
   else {
-    return 0.0 ;
+    return 2.0 ;
   }
 }
 
@@ -48,25 +48,25 @@ double IDensity(double x, double y) {
     return 1.0;
   }
   else if ( x > 0.0 && y > 0.0) {
-    return 0.125 ;
+    return 1.0 ;
   }
   else {
-    return 0.125 ;
+    return 1.0 ;
   }
 }
 
 double IPressure(double x, double y) {
   if ( x <= 0.0 && y <= 0.0 ) {
-    return 1.0 ;
+    return 0.4 ;
   }
   else if ( x <= 0.0 && y > 0.0) {
-    return 1.0;
+    return 0.4;
   }
   else if ( x > 0.0 && y > 0.0) {
-    return 0.1 ;
+    return 0.4 ;
   }
   else {
-    return 0.1 ;
+    return 0.4 ;
   }
 }
 
@@ -76,16 +76,16 @@ double StateEq(double D, double T) {
 
 double ITemperature(double x, double y) {
   if ( x <= 0.0 && y <= 0.0 ) {
-    return 1.0/(R*1.0) ;
+    return 0.4/(R*1.0) ;
   }
   else if ( x <= 0.0 && y > 0.0) {
-    return 1.0/(R*1.0) ;
+    return 0.4/(R*1.0) ;
   }
   else if ( x > 0.0 && y > 0.0) {
-    return 0.1/(R*0.125) ;
+    return 0.4/(R*1.0) ;
   }
   else {
-    return 0.1/(R*0.125) ;
+    return 0.4/(R*1.0) ;
   }
 }
 
@@ -103,16 +103,16 @@ double Sound( double D, double P) {
 
 // Analytical solutions of Density and Pressure at t = 0.2 secs, for 1D Sod's Shock Tube
 double AnalyticalDensity(double x, double y) {
-  if (-1.0 <= x && x <= -0.48) {
+  if (-1.0 <= x && x <= -0.478) {
     return 1.0 ;
   }
-  else if (-0.48 <= x && x <= -0.03) {
-    return 1.0 + (x+0.48)*(0.42-1.0)/(-0.03+0.48) ;
+  else if (-0.478 <= x && x <= -0.025) {
+    return 1.0 + (x+0.478)*(0.42-1.0)/(-0.025+0.478) ;
   }
-  else if (-0.03 <= x && x <= 0.37) {
+  else if (-0.025 <= x && x <= 0.37) {
     return 0.42 ;
   }
-  else if (0.37 < x && x <= 0.71) {
+  else if (0.37 < x && x <= 0.70) {
     return 0.27 ;
   }
   else {
@@ -121,14 +121,14 @@ double AnalyticalDensity(double x, double y) {
 }
 
 double AnalyticalVelocity(double x, double y) {
-  if (-1.0 <= x && x <= -0.48) {
+  if (-1.0 <= x && x <= -0.478) {
     return 0.0 ;
   }
-  else if (-0.48 <= x && x <= -0.03) {
-    return (x+0.48)*(0.93)/(-0.03+0.48) ;
+  else if (-0.478 <= x && x <= -0.025) {
+    return (x+0.478)*(0.925)/(-0.025+0.48) ;
   }
-  else if (-0.03 <= x & x <= 0.71) {
-    return 0.93 ;
+  else if (-0.025 <= x & x <= 0.7) {
+    return 0.925 ;
   }
   else {
     return 0.0 ;
@@ -138,9 +138,9 @@ double AnalyticalVelocity(double x, double y) {
 int main() {
     clock_t tstart = clock();
     double dt = 1e-3;
-    int time_steps = 400;
+    int time_steps = 300;
     EulerSolver* a;
-    a = new EulerSolver(20, 5, 2);
+    a = new EulerSolver(20, 10, 2);
     a->setDomain(-1.0, -1.0, 1.0, 1.0);
 
     a->setInitialVelocity(U, V);
@@ -154,7 +154,7 @@ int main() {
     a->SetLimiter("LiliaMoment");
     a->setSolver(dt, time_steps);
     a->solve( Sound, T, StateEq, IE);
-    a->FindL2Norm(AnalyticalDensity, AnalyticalVelocity);
+    //a->FindL2Norm(AnalyticalDensity, AnalyticalVelocity);
     a->plot("output.vtk");
     
 
