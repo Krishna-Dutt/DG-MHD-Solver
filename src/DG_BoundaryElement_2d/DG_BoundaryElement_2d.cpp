77ca3a3a72158f1c71e3f3cd387e9e647cbb7a09
KrishnaDutt <kdutt0592@gmail.com>
2018-01-21 11:09:35 +0530

    Corrected BoundaryMethods

--- src/DG_BoundaryElement_2d/DG_BoundaryElement_2d.cpp
+++ src/DG_BoundaryElement_2d/DG_BoundaryElement_2d.cpp
@@ -589,7 +589,7 @@ void DG_BoundaryElement_2d::limitMoments(int *M, int *Modm, unsigned Index, unsi
           --count;
           //AlphaN = sqrt((2.0*(count)-1.0)/(2.0*(count)+1.0)); 
           //AlphaN = sqrt((2.0*(count)-1.0)/(2.0*(count)+1.0));
-          AlphaN = 0.5*sqrt((4.0*(count)-1.0)/(2.0*(count)+1.0));
+          //AlphaN = 0.5*sqrt((4.0*(count)-1.0)/(2.0*(count)+1.0));
           //AlphaN = 0.5/sqrt(4.0*count*count -1.0);
           for(j=0; j < count && counter == 0; ++j) {
              Tempi = i-j;
