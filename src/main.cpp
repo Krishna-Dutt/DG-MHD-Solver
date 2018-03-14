51693c1b18c3fa718a12d264bca62292a5d741f7
KrishnaDutt <kdutt0592@gmail.com>
2018-03-14 14:42:27 +0530

    Testing for 200x200, N2

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 #include <ctime>
 
-#define PARALLEL false
+#define PARALLEL true
 
 using namespace std;
 
@@ -123,14 +123,14 @@ double AnalyticalVelocity(double x, double y) {
 }
 
 int main(int argc, char **argv) {
-    if(PARALLEL) omp_set_num_threads(2);
+    if(PARALLEL) omp_set_num_threads(8);
     clock_t tstart = clock();
     //double dt = 0.5e-3;
     int time_steps = 10;
     double CFL = 0.2;
     double time = 0.8;
     IdealMHDSolver* a;
-    a = new IdealMHDSolver(120, 120, 2);
+    a = new IdealMHDSolver(200, 200, 2);
     a->setDomain(-0.4, -0.4, 0.4, 0.4);
     a->setBoundaryCondtions("neumann", "neumann", "neumann", "neumann");
     a->setSolver(CFL, time, time_steps);
@@ -156,7 +156,7 @@ int main(int argc, char **argv) {
     a->SetLimiter("CharacteristicLimiter");
     a->solve();
     a->FindL2Norm(IDensity, U);
-    a->plot("2DRP6MHD1.vtk");
+    a->plot("2DRP6MHD.vtk");
     
 
     delete a;
