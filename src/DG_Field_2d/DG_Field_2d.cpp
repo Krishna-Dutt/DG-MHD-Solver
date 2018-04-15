0eea2e0c571a9a49a708447490fb86dbc6ef03ba
KrishnaDutt <kdutt0592@gmail.com>
2018-04-15 12:33:52 +0530

    Running for N=1

--- src/DG_Field_2d/DG_Field_2d.cpp
+++ src/DG_Field_2d/DG_Field_2d.cpp
@@ -56,12 +56,12 @@ DG_Field_2d::DG_Field_2d(int _nex, int _ney, int _N, double _x1, double _y1, dou
     // Setting up exponential grid (noo-uniform grid) along y direction
     double Beta_y, Beta_x1, Beta_x2, DeltaX1, DeltaX2, DeltaY, epsilon = 1e-10;
     // Setting up Hyperbolic grids along x-direction about scale_b X (x2-x1)
-    scale_b = 0.4;
+    scale_b = 0.5;
     DeltaY = (y2-y1);
-    Beta_y = 1.17;
+    Beta_y = 1.081;
     
     DeltaX1 = (x2-x1)*scale_b;
-    Beta_x1 = 1/1.04;
+    Beta_x1 = 1/1.0004;
     dx = DeltaX1 * (Beta_x1 - 1.0 + epsilon)/(pow(Beta_x1, ne_x*scale_b) -1.0 + epsilon);
     
     for(int i=0; i<ne_x*scale_b; i++){
@@ -84,7 +84,7 @@ DG_Field_2d::DG_Field_2d(int _nex, int _ney, int _N, double _x1, double _y1, dou
     } // All the elements have been initialized.
 
     DeltaX2 = (x2-x1)*(1-scale_b);
-    Beta_x2 = 1.04;
+    Beta_x2 = 1.0004;
     dx = DeltaX2 * (Beta_x2 - 1.0 + epsilon)/(pow(Beta_x2, ne_x*(1-scale_b)) -1.0 + epsilon);
 
     for(int i=ne_x*scale_b; i<ne_x; i++){
