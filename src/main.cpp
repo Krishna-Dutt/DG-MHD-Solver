4b8d4e1a79644ac5174d29e4d8db97cd5e4ef042
KrishnaDutt <kdutt0592@gmail.com>
2018-04-15 18:27:14 +0530

    Running with 100 elements along x dirction

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ int main(int argc, char **argv) {
     double CFL = 0.6;
     double time = 7*8e-3;
     NSSolver* a;
-    a = new NSSolver(160, 80, 1);
+    a = new NSSolver(100, 40, 2);
     a->setDomain(0.0, 0.0, 2.0, 1.1);
     a->setBoundaryCondtions("AdiabaticWall", "neumann", "dirichlet", "dirichlet");
     a->setSolver(CFL, time, time_steps);
@@ -106,7 +106,7 @@ int main(int argc, char **argv) {
     //a->SetLimiter("CharacteristicLimiter");
     a->solve();
     a->FindL2Norm(IDensity, U);
-    a->plot("ShockBLInteractionTest_N1_160x80_HBGrid_times7.vtk");
+    a->plot("ShockBLInteractionTest_N2_100x40_HBGrid_times7.vtk");
     
     delete a;
     cout << "Time Taken :: "<< (double)(clock() - tstart)/CLOCKS_PER_SEC <<"\n";
